8f9b618fd80b72e3828b8025511a9d18ede6532a
max <maksim.levental@gmail.com>
2023-07-14 13:51:57 -0500

    wrap upstream ops (trampoline to them)

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -3,12 +3,15 @@ import pytest
 # noinspection PyUnresolvedReferences
 from mlir_utils.testing import filecheck, mlir_ctx as ctx
 from mlir_utils.types import i32_t
+# noinspection PyUnresolvedReferences
+from triton_air.dialects.ext import triton
+from mlir_utils.dialects import triton as tt
 from triton_mlir_bindings.dialects import (
     arith as arith_dialect,
 )
 from triton_mlir_bindings.ir import IntegerType
 
-from triton_air.dialects import air, triton as tt
+from triton_air.dialects import air
 
 pytest.mark.usefixtures("ctx")
 
@@ -32,7 +35,7 @@ def test_smoke_with_triton(ctx):
     @tt.FuncOp.from_py_func(results=[])
     def kernel_0123():
         c64 = arith_dialect.ConstantOp(i32, 64)
-        v0 = tt.GetProgramIdOp(axis=0)
+        v0 = tt.GetProgramIdOp(axis="x")
         air.ChannelOp("bob")
         tt.ReturnOp([])
 
@@ -40,7 +43,7 @@ def test_smoke_with_triton(ctx):
     module {
       tt.func @kernel_0123() {
         %c64_i32 = arith.constant 64 : i32
-        %0 = tt.get_program_id {axis = 0 : i32} : i32
+        %0 = tt.get_program_id x : i32
         "air.channel"() {sym_name = "bob"} : () -> ()
         tt.return
       }
--- tests/test_types.py
+++ tests/test_types.py
@@ -6,21 +6,13 @@ import pytest
 from triton_mlir_bindings.dialects import triton as triton_dialect
 
 from triton_air.dialects.ext import triton
-from triton_air.dialects.ext.triton import (
-    splat,
-    arange,
-    addptr,
-    load,
-    store,
-    program_id,
-)
+from triton_air.dialects.ext.triton import splat, arange, addptr, load, store
 from triton_air.types import ptr, get_ptr_type
 
 # this needs to be below the triton_mlir_bindings
-from mlir_utils.dialects.ext import arith
-from mlir_utils.dialects.ext.arith import constant
-
 # noinspection PyUnresolvedReferences
+from mlir_utils.dialects.ext import arith
+from mlir_utils.dialects import triton as tl
 from mlir_utils.testing import filecheck, mlir_ctx as ctx, MLIRContext
 from mlir_utils.types import i32_t, f32_t
 
@@ -32,50 +24,43 @@ def test_ptr_type(ctx: MLIRContext):
     p_f32_t = ptr(f32_t)
     assert f32_t.isinstance(get_ptr_type(p_f32_t))
 
-    BLOCK_SIZE = 64
-
     @triton.jit
-    def add_kernel(
-        x_ptr: p_f32_t, y_ptr: p_f32_t, output_ptr: p_f32_t, n_elements: i32_t
-    ):
-        pid = program_id(axis=0)
+    def kernel_0123(arg0: p_f32_t, arg1: p_f32_t, arg2: p_f32_t, arg3: i32_t):
+        v0 = tl.get_program_id(axis="x")
+        c32 = arith.constant(64, i32_t)
+        # doesn't until triton catches up to
+        # https://github.com/llvm/llvm-project/commit/bfb1ba752655bf09b35c486f6cc9817dbedfb1bb
+        # v1 = v0 * c32
+        v1 = arith.muli(v0, c32)
+        v2 = arange(0, 64)
+        v3 = splat(v1, (64,))
+        v4 = arith.addi(v3, v2)
+        v5 = splat(arg3, (64,))
+        v6 = arith.cmpi("slt", v4, v5)
+        v7 = splat(arg0, (64,))
+        v8 = addptr(v7, v4)
+        v9 = load(v8, v6, cache="none", evict="normal", is_volatile=False)
+        v10 = splat(arg1, (64,))
+        v11 = addptr(v10, v4)
+        v12 = load(v11, v6, cache="none", evict="normal", is_volatile=False)
+        v13 = arith.addf(v9, v12)
+        v14 = splat(arg2, (64,))
+        v15 = addptr(v14, v4)
+        store(v15, v13, v6)
 
-        block_start = arith.muli(pid, constant(BLOCK_SIZE, i32_t))
-        block_start = splat(block_start, (BLOCK_SIZE,))
-
-        i2 = arange(0, BLOCK_SIZE)
-        offsets = arith.addi(block_start, i2)
-
-        n_elements = splat(n_elements, (BLOCK_SIZE,))
-        mask = arith.cmpi("slt", offsets, n_elements)
-
-        x_ptr = splat(x_ptr, (BLOCK_SIZE,))
-        x_ptr_plus_offsets = addptr(x_ptr, offsets)
-        x = load(x_ptr_plus_offsets, mask)
-
-        y_ptr = splat(y_ptr, (BLOCK_SIZE,))
-        y_ptr_plus_offsets = addptr(y_ptr, offsets)
-        y = load(y_ptr_plus_offsets, mask)
-
-        output = arith.addf(x, y)
-        output_ptr = splat(output_ptr, (BLOCK_SIZE,))
-        output_ptr = addptr(output_ptr, offsets)
-
-        store(output_ptr, output, mask)
-
-    add_kernel.emit()
+    kernel_0123.emit()
 
     ctx.module.operation.verify()
     correct = dedent(
         """\
     module {
-      tt.func @add_kernel(%arg0: !tt.ptr<f32>, %arg1: !tt.ptr<f32>, %arg2: !tt.ptr<f32>, %arg3: i32) {
-        %0 = tt.get_program_id {axis = 0 : i32} : i32
+      tt.func @kernel_0123(%arg0: !tt.ptr<f32>, %arg1: !tt.ptr<f32>, %arg2: !tt.ptr<f32>, %arg3: i32) {
+        %0 = tt.get_program_id x : i32
         %c64_i32 = arith.constant 64 : i32
         %1 = arith.muli %0, %c64_i32 : i32
-        %2 = tt.splat %1 : (i32) -> tensor<64xi32>
-        %3 = tt.make_range {end = 64 : i32, start = 0 : i32} : tensor<64xi32>
-        %4 = arith.addi %2, %3 : tensor<64xi32>
+        %2 = tt.make_range {end = 64 : i32, start = 0 : i32} : tensor<64xi32>
+        %3 = tt.splat %1 : (i32) -> tensor<64xi32>
+        %4 = arith.addi %3, %2 : tensor<64xi32>
         %5 = tt.splat %arg3 : (i32) -> tensor<64xi32>
         %6 = arith.cmpi slt, %4, %5 : tensor<64xi32>
         %7 = tt.splat %arg0 : (!tt.ptr<f32>) -> tensor<64x!tt.ptr<f32>>
