66606f36b89a4350731f7ec1ba1a2a24ce39363b
max <maksim.levental@gmail.com>
2023-08-10 00:03:30 -0500

    redirect

--- examples/demo.py
+++
@@ -1,8 +0,0 @@
-from triton_mlir_bindings.util.utils import mlir_mod_ctx
-
-from triton_air.dialects import air
-
-with mlir_mod_ctx(allow_unregistered_dialects=True) as ctx:
-    x = air.channel("bob")
-
-print(ctx)
--- triton_air/__init__.py
+++
@@ -1,6 +0,0 @@
-from mlir_utils import DefaultContext
-from triton_mlir_bindings.dialects import triton as triton_dialect
-
-triton_dialect.register_dialect(DefaultContext)
-
-from .dialects.ext.triton import TritonTensor
